f42e830731d2aead72ebed1b9ced9a654faa2107
c. sooriyakumaran <c.sooriyakumaran@gmail.com>
2022-08-24 10:23:58 -0400

    removing imgui node editor

--- venturi/premake5.lua
+++ venturi/premake5.lua
@@ -20,7 +20,7 @@ project "venturi"
         "../oak/vendor",
         "../oak/vendor/implot",
         "../oak/vendor/imgui",
-        "../oak/vendor/imnode"
+        "../oak/vendor/imnode",
         "../oak/vendor/spdlog/include",
         "../oak/vendor/glad/include",
         "../oak/vendor/glfw/include",
